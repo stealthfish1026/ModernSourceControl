abf37587e1530f99b5ba0198b9ca850163b8944d
stealthfish1026 <92447732+stealthfish1026@users.noreply.github.com>
2022-07-12 19:08:54 +0300

    Updated the startup.cs file

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -21,6 +21,7 @@ namespace WebApplication1
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // new comment.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
